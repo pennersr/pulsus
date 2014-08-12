3b9dce8209f094d040454bfc545e707cdfdfa01e
Raymond Penners <raymond.penners@intenct.nl>
2014-08-12 14:17:21 +0200

    Skeleton code for result handing

--- pulsus/services/gcm/service.py
+++ pulsus/services/gcm/service.py
@@ -23,11 +23,57 @@ class GCMService(BaseService):
                              data=message.pack(),
                              headers=headers)
         resp = grequests.map([req])[0]
-        print resp.status_code
-        print resp.content
-        print resp.headers
-        # Update auth token
-        # Inspect id/error retry-after
-        # https://groups.google.com/group/android-c2dm/browse_thread/thread/00b03b6a0985059f/14b69680f3b6926b  # noqa
+        resp.raise_for_status()
+        data = resp.json()
+        # Example:
+        # {"multicast_id":592394215791271011422,
+        #  "success":1,"failure":0,"canonical_ids":0,
+        #  "results":[{"message_id":"0:12094e2028990994746%975ba952f9fd7ecd"}]}
 
-        return resp
+        # If the value of failure and canonical_ids is 0, it's not
+        # necessary to parse the remainder of the response.
+        if not data['failure'] and not data['canonical_ids']:
+            return
+        for result in data['results']:
+            logger.info('Result: %r' % result)
+            # message_id = result['message_id']
+            registration_id = result.get('registration_id')
+            error = result.get('error')
+            if registration_id:
+                # If registration_id is set, replace the original ID
+                # with the new value (canonical ID) in your server
+                # database. Note that the original ID is not part of
+                # the result, so you need to obtain it from the list
+                # of code>registration_ids passed in the request
+                # (using the same index).
+
+                # FIXME
+                pass
+            if error:
+                if error == 'Unavailable':
+                    # If it is Unavailable, you could retry to send it in
+                    # another request.
+
+                    # FIXME
+                    pass
+                elif error == 'NotRegistered':
+                    # {u'error': u'NotRegistered'}
+
+                    # you should remove the registration ID from your
+                    # server database because the application was
+                    # uninstalled from the device or it does not have a
+                    # broadcast receiver configured to receive
+                    # com.google.android.c2dm.intent.RECEIVE intents.
+
+                    # FIXME
+                    pass
+                else:
+                    # Otherwise, there is something wrong in the
+                    # registration ID passed in the request; it is
+                    # probably a non-recoverable error that will also
+                    # require removing the registration from the server
+                    # database. See Interpreting an error response for all
+                    # possible error values.
+
+                    # FIXME
+                    pass
