17c467c6b8b3c26200f17c6eb87d4352d14391c1
Raymond Penners <raymond.penners@intenct.nl>
2014-10-31 21:57:00 +0100

    Releasing 0.1.0

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import io
 from setuptools import setup, find_packages
 
 requires = [
@@ -6,12 +7,30 @@ requires = [
     'grequests'
 ]
 
+long_description = io.open('README.rst', encoding='utf-8').read()
+
 setup(
     name='pulsus',
     version='0.1.0',
     author="Raymond Penners",
     author_email="raymond.penners@intenct.nl",
+    description='Push Notification Service handling Apple Push'
+    ' Notification Service (APNS), and Google Cloud Messaging (GCM).',
+    long_description=long_description,
+    url='https://github.com/pennersr/pulsus',
+    keywords='push notifications apns gcm',
     packages=find_packages(),
     install_requires=requires,
     zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Environment :: Web Environment',
+        'Topic :: Internet',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7'
+    ],
 )
